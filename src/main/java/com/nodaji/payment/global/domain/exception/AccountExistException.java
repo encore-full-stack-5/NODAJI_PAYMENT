5fbdce2f5f221b3d5d44f92f42d117b6600992dd
parkhyunseo <dazzle1999@naver.com>
2024-06-12 23:32:21 +0900

    [feat]: 계좌 삭제 로직 추가

--- src/main/java/com/nodaji/payment/global/domain/exception/AccountExistException.java
+++ src/main/java/com/nodaji/payment/global/domain/exception/AccountExistException.java
@@ -2,6 +2,6 @@ package com.nodaji.payment.global.domain.exception;
 
 public class AccountExistException extends IllegalArgumentException{
     public AccountExistException() {
-        super("Account Already Exist");
+        super("해당 계좌가 이미 존재합니다.");
     }
 }
--- src/main/java/com/nodaji/payment/global/domain/exception/UserNotFoundException.java
+++ src/main/java/com/nodaji/payment/global/domain/exception/UserNotFoundException.java
@@ -2,6 +2,6 @@ package com.nodaji.payment.global.domain.exception;
 
 public class UserNotFoundException extends IllegalArgumentException{
     public UserNotFoundException() {
-        super("User Not Found Exception");
+        super("해당 유저를 찾을 수 없습니다.");
     }
 }
