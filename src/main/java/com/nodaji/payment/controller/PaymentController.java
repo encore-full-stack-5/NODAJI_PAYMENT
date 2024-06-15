7d74cf38a954e63c57769ef1e35b223bd4aac958
parkhyunseo <dazzle1999@naver.com>
2024-06-15 12:09:10 +0900

    [feat]: [refactor]: 토스 결제 리팩터링

--- src/main/java/com/nodaji/payment/controller/PaymentController.java
+++ src/main/java/com/nodaji/payment/controller/PaymentController.java
@@ -1,15 +1,10 @@
 package com.nodaji.payment.controller;
 
 import com.nodaji.payment.dto.response.PaymentErrorResponseDto;
-import com.nodaji.payment.dto.response.PaymentSuccessResponseDto;
-import com.nodaji.payment.service.AccountService;
-import com.nodaji.payment.service.PaymentHistoryService;
 import com.nodaji.payment.service.PaymentService;
 import lombok.RequiredArgsConstructor;
-import org.json.simple.JSONObject;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,31 +12,19 @@ import java.util.Map;
 public class PaymentController {
 
     private final PaymentService paymentService;
-    private final PaymentHistoryService paymentHistoryService;
-    private final AccountService accountService;
 
     @GetMapping("/success/{userId}")
+    @ResponseStatus(HttpStatus.OK)
     public Object submitPayment(
             @PathVariable(name = "userId") String userId,
             @RequestParam(name = "orderId") String orderId,
             @RequestParam(name = "amount") Long amount,
             @RequestParam(name = "paymentKey") String paymentKey) throws Exception {
-
-        Map<String, Object> result = paymentService.processPayment(userId, orderId, amount, paymentKey);
-        boolean isSuccess = (boolean) result.get("isSuccess");
-        JSONObject jsonObject = (JSONObject) result.get("response");
-        if (isSuccess) {
-//            예치금 추가
-            accountService.depositPoint(userId,amount);
-//            결제 내역 저장
-            paymentHistoryService.createPaymentHistory(jsonObject, userId);
-            return PaymentSuccessResponseDto.fromJSONObject(jsonObject);
-        } else {
-            return PaymentErrorResponseDto.fromJSONObject(jsonObject);
-        }
+        return paymentService.processPayment(userId, orderId, amount, paymentKey);
     }
 
     @GetMapping("/fail")
+    @ResponseStatus(HttpStatus.CONFLICT)
     public PaymentErrorResponseDto paymentResult(
             @RequestParam(value = "message") String message,
             @RequestParam(value = "code") Integer code) {
